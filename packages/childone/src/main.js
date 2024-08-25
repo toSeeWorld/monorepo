08022e67279929b0a701387dd9b931f95e18323c
sundingjia1 <sundingjia1@jd.com>
2024-08-25 23:31:58 +0800

    fix:rm console

--- packages/childone/src/main.js
+++ packages/childone/src/main.js
@@ -6,7 +6,6 @@ import routes from './router';
 const render = (props) => {
   const { container, baseRoute } = props
   const app = createApp(App)
-  console.log('hhhs')
   const router = createRouter({
     history: createWebHistory(baseRoute),
     routes
